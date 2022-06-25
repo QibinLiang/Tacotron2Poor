67898922f0df1f3bca070b23d6692283aab9ad9e
qibin liang <physechan@gmail.com>
2022-06-25 10:30:30 +0800

    add padding function

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import torch as tr
 import json
 from torchaudio import transforms as T
 import torchaudio
+import config
+import torch
 
 
 class CustomDataset(Dataset):
@@ -20,23 +22,60 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, index):
         key, val = self.data[index]
-        # get mel_spec
+
         wav_path = val['wav']
         # get transcript
         wrd = val['wrd']
+        wrd = tr.LongTensor(character_tokenizer(wrd, config.token2id))
         # get transcript length
         wrd_len = len(wrd)
         # load wav
         wav, sr = torchaudio.load(wav_path)
-        padded_mel, target_stop = self.compute_padded_mel(wav, wrd_len, self.n_mels)
-        return padded_mel, target_stop , wrd, wrd_len
+        # get mel_spec
+        padded_mel, target_mel_spec_len, target_stop = self.compute_mel(wav, wrd_len, self.n_mels)
+        padded_mel = padded_mel.squeeze(0).permute(1, 0)
+        return padded_mel, target_mel_spec_len, target_stop, wrd, wrd_len
 
-    def compute_padded_mel(self, wav, wrd_len, n_mels, frames_per_char=6):
-        frames = wrd_len * frames_per_char
-        padded_mel = tr.zeros(n_mels, frames)
-        target_stop = tr.ones(n_mels, frames)
+    def compute_mel(self, wav, wrd_len, n_mels):
         mel_spec = self.fbank(wav)
-        target_mel_spec_len = mel_spec.shape[1]
-        padded_mel[:, :target_mel_spec_len] = mel_spec
-        target_stop[:, target_mel_spec_len:] = 0.0
-        return padded_mel, target_stop
+        target_stop = tr.ones(mel_spec.shape[-1])
+        target_mel_spec_len = mel_spec.shape[2]
+        return mel_spec, target_mel_spec_len, target_stop
+
+
+def character_tokenizer(wrd, token2id):
+    ids = []
+    for token in wrd:
+        ids.append(token2id[token])
+    return ids
+
+
+def convert_token_char(tokens, token2id):
+    id2token = {token2id[key]: key for key in token2id.keys()}
+    wrds = [id2token[token] for token in tokens]
+    return wrds
+
+
+def collate_fn_pad(batch):
+    padded_mel, frames, target_stop, wrd, wrd_len = zip(*batch)
+    # get the target length
+    target_lens = tr.tensor([f for f in frames])
+    # compat all mels
+    mel_batch = [tr.Tensor(t) for t in padded_mel]
+    # pad mels again
+    mel_batch = tr.nn.utils.rnn.pad_sequence(mel_batch)
+
+    # pad target_stop
+    target_stop = tr.nn.utils.rnn.pad_sequence(target_stop,batch_first=True)
+    # pad wrd
+    wrd = tr.nn.utils.rnn.pad_sequence(wrd, batch_first=True)
+    wrd_len = tr.tensor([l for l in wrd_len])
+    batch = mel_batch, target_lens, target_stop, wrd, wrd_len
+    return batch
+
+
+#dataset = CustomDataset('dev.json')
+#dataloader = DataLoader(dataset, batch_size=10, shuffle=True, collate_fn=collate_fn_pad)
+#mel_outputs, mel_outputs_res, stop_tokens = batch = next(iter(dataloader))
+
+#print(mel_outputs.shape, mel_outputs_res.shape, stop_tokens.shape)
